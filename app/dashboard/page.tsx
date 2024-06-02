c124f502e40c2821868b733b962ae452fef5f17d
faisal seraj <faisalseraj47@gmail.com>
2024-06-02 17:48:46 +0500

    initial  commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Page(){
+    return <p>Dashboard page
+        <br/>
+
+        <a href='/dashboard/customer'>customer</a>
+        <br/>
+        <a href='/dashboard/invoice'>invoice</a>
+    </p>
+}
